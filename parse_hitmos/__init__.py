e485c194f471338cf92854f01a27f267df758ed2
za999919999-byte <z.a999919999@gmail.com>
2026-05-01 23:20:11 +0600

    Create __init__.py

---
+++ parse_hitmos/__init__.py
@@ -0,0 +1,6 @@
+from parse_hitmos.entered_tracks import EnteredTrack
+from parse_hitmos.rating_tracks_count import RatingCount
+from parse_hitmos.rating_tracks_page import RatingPage
+
+__author__ = 'JoyHubN'
+__version__= '1.0.0'
